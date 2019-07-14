515c9fc8cb8c8df87acebda8eb71ad2343c65746
Grandduchy <jchallenger1@hotmail.com>
2019-07-14 19:40:53 -0400

    added asbyte for status

--- src/State6502.cpp
+++ src/State6502.cpp
@@ -15,3 +15,15 @@ void State6502::clear() {
 void State6502::Status::clear() noexcept {
     c = z = i = d = b = pad = o = n = 0;
 }
+
+uint8_t State6502::Status::asByte() const noexcept {
+    uint8_t byte = 0;
+    byte |= c;
+    byte |= z << 1;
+    byte |= i << 2;
+    byte |= d << 3;
+    // 4 & 5 are the B(not break) flag that has no CPU effect. https://wiki.nesdev.com/w/index.php/Status_flags#The_B_flag
+    byte |= o << 6;
+    byte |= n << 7;
+    return byte;
+}
