d0bc801cc073510c07d5ee5b82f2d9e06a461564
Grandduchy <jchallenger1@hotmail.com>
2019-12-23 12:06:39 -0500

    remove exception handling for now

--- src/main.cpp
+++ src/main.cpp
@@ -11,29 +11,15 @@ int main(int argc, char *argv[]) {
 
     QApplication a(argc, argv);
 
-    try {
-        std::shared_ptr<NES> nes = std::make_shared<NES>();
-        nes->init();
-        GamePak::load(nes->cpu.memory, nes->ppu, "../YaNES/rsc/roms/Donkey Kong (World) (Rev A).nes");
-        nes->powerUp();
+    std::shared_ptr<NES> nes = std::make_shared<NES>();
+    nes->init();
+    GamePak::load(nes->cpu.memory, nes->ppu, "../YaNES/rsc/roms/Donkey Kong (World) (Rev A).nes");
+    nes->powerUp();
 
-        MainWindow main(nes->getPtr());
-        NameTableView name(nes->getPtr(), false);
-        main.show();
-        name.show();
-    } catch (const QException& e) {
-        std::cerr << e.what() << std::endl;
-        return -1;
-    }
-    catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
-        return -1;
-    }
-    catch (...) {
-        std::cerr << "Fatal Error caught out of main" << std::endl;
-        return -1;
-    }
-
+    MainWindow main(nes->getPtr());
+   // NameTableView name(nes->getPtr(), false);
+    main.show();
+    //name.show();
     return a.exec();
 
 }
